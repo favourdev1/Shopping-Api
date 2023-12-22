ebde05ab07e7a64b62a785173140f0bcf480df4e
favourdev1 <favourapps17@gmail.com>
2023-12-22 05:17:17 +0100

    Changed the POST route for products from '/products' to '/products/add'

--- routes/api.php
+++ routes/api.php
@@ -89,7 +89,7 @@ Route::middleware('auth:api')->group(function () {
         Route::prefix('products')->group(function () {
             Route::get('/', [ProductController::class, 'index']);
             Route::get('/{product}', [ProductController::class, 'show']);
-            Route::post('/', [ProductController::class, 'store']);
+            Route::post('/add', [ProductController::class, 'store']);
             Route::put('/{product}', [ProductController::class, 'update']);
             Route::delete('/{product}', [ProductController::class, 'destroy']);
         });
